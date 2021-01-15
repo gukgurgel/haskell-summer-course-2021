f881d34071fba27176bea433b241113c008054b8
Gustavo Korzune Gurgel <gukgurgel@usp.br>
2021-01-15 18:10:48 -0300

    Add solution to problem 3.2
    Signed-off-by: Gustavo Korzune Gurgel <gukgurgel@usp.br>

--- first_assignment/part_a/chap_3.hs
+++ first_assignment/part_a/chap_3.hs
@@ -5,9 +5,14 @@ data Temperatura = Celsius | Fahrenheit | Kelvin
 converterCelsius :: Double -> Temperatura -> Double
 converterCelsius x Celsius = x
 converterCelsius x Fahrenheit = (x - 32) * 5/9
-converterCelsius x Kelvin = x + 273
+converterCelsius x Kelvin = x - 273
 
 converterKelvin :: Double -> Temperatura -> Double
-converterKelvin x Celsius = x - 273
+converterKelvin x Celsius = x + 273
 converterKelvin x Fahrenheit = converterKelvin (converterCelsius x Fahrenheit) Celsius
 converterKelvin x Kelvin = x
+
+converterFahrenheit :: Double -> Temperatura -> Double
+converterFahrenheit x Celsius = (x * 9/5) + 32
+converterFahrenheit x Fahrenheit = x
+converterFahrenheit x Kelvin = converterFahrenheit (converterCelsius x Kelvin) Celsius
