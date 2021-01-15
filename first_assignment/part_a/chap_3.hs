d03b1de8bb96d2f4f67a4c3cf2c667a60043eba3
Gustavo Korzune Gurgel <gukgurgel@usp.br>
2021-01-15 18:52:17 -0300

    Add Problem 3.11
    Signed-off-by: Gustavo Korzune Gurgel <gukgurgel@usp.br>

--- first_assignment/part_a/chap_3.hs
+++ first_assignment/part_a/chap_3.hs
@@ -1,4 +1,4 @@
----- Questão 3.2 ----
+-- Problem 3.2
 
 data Temperatura = Celsius | Fahrenheit | Kelvin
 
@@ -16,3 +16,21 @@ converterFahrenheit :: Double -> Temperatura -> Double
 converterFahrenheit x Celsius = (x * 9/5) + 32
 converterFahrenheit x Fahrenheit = x
 converterFahrenheit x Kelvin = converterFahrenheit (converterCelsius x Kelvin) Celsius
+
+-- Problem 3.11
+
+data Binario = Zero | Um deriving (Enum, Eq, Show)
+data Funcao = Soma2 | Maior | Menor | Mult2
+aplicar :: Funcao -> Binario -> Binario -> Binario
+aplicar Soma2 Um Um = Zero
+aplicar Soma2 Zero Zero = Zero
+aplicar Soma2 _ _ = Um
+
+aplicar Mult2 Um Um = Um
+aplicar Mult2 _ _ = Zero
+
+aplicar Maior Zero Zero = Zero
+aplicar Maior x y = Um
+
+aplicar Menor Um Um = Um
+aplicar Menor x y = Zero
