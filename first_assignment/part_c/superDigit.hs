1ae12cad7c31c062b414e8664571d9c0282ccd06
Guilhermo Costa <guillecosta@protonmail.ch>
2021-01-16 23:32:08 -0300

    Solve Hackerrank Super Digit problem

---
+++ first_assignment/part_c/superDigit.hs
@@ -0,0 +1,25 @@
+import Data.List
+import Control.Arrow ((>>>))
+
+type Digit = Int
+
+base :: Integer
+base = 10
+
+digitSum :: Integer -> Integer
+digitSum n = aux n 0
+    where aux n acc
+            | n < base = n + acc
+            | otherwise = aux (n `div` base) (acc + n `mod` base)
+
+superDigit :: Integer -> Integer
+superDigit n
+  | n < base = n
+  | otherwise = superDigit $ digitSum n
+
+solve :: [Integer] -> Integer
+solve [n, k] = superDigit $ k * digitSum n
+
+main :: IO ()
+main = interact $
+    words >>> map read >>> solve >>> show
